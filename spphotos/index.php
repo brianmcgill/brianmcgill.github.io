90c0abe349d1d3d81749af253c56359cd909ef76
brianmcgill <mubear22@hotmail.com>
2023-04-18 00:51:23 -0400

    landing page php

---
+++ spphotos/index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+  // Start a PHP block
+  echo "<style>\n";
+  // Define the CSS class
+  echo "img {\n";
+  echo "width: 100%;\n";
+  echo "}\n";
+
+  echo ".boxz {\n";
+  echo "width: 200px;\n";
+  echo "height: 150px;\n";
+  echo "float:left;\n";
+  echo "margin-right:10px;\n";
+  echo "margin-bottom:10px;\n";
+  echo "}\n";
+  // End the CSS block
+  echo "</style>\n";
+
+
+$path = "."; //images/pathname.jpg
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != "index.html"&& $file != ".htaccess" && $file != "error_log" && $file != ".DS_Store") {
+        echo "<div class='boxz'>
+                 <a href='$file'><img src='$file'></a><br /><br />
+              </div>";
+        //echo "<a href='$path/$file'>$file</a><br /><br />";
+
+        $i++;
+    }
+}
+closedir($dh);
+?> 
